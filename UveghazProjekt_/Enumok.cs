d8ec244c0d85e2ab1484a87c180cc3a7b0184342
Mariann <mariann.bogdan@petrik.hu>
2026-02-20 09:16:38 +0100

    Kezelo osztaly kesz

---
+++ UveghazProjekt_/Enumok.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UveghazProjekt_
+{
+	//	Szerepkor: Kertesz, Technikus, Admin
+	enum Szerepkor
+	{
+		KERTESZ,
+		TECHNIKUS,
+		ADMIN
+	}
+
+	internal class Enumok
+	{
+	}
+}
--- UveghazProjekt_/Kezelo.cs
+++ UveghazProjekt_/Kezelo.cs
@@ -8,5 +8,24 @@ namespace UveghazProjekt_
 {
 	internal class Kezelo
 	{
+		string nev;
+		string azonosito;
+		Szerepkor szerepkor;
+
+		public Kezelo(string nev, string azonosito, Szerepkor szerepkor)
+		{
+			this.nev = nev;
+			this.azonosito = azonosito;
+			this.szerepkor = szerepkor;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public string Azonosito { get => azonosito; set => azonosito = value; }
+		internal Szerepkor Szerepkor { get => szerepkor; set => szerepkor = value; }
+
+		public override string ToString()
+		{
+			return $"{this.nev} ({this.azonosito}) {this.szerepkor}";
+		}
 	}
 }
