9a9ad633a70576f4b6ec92a27d6a31812ae1b917
Mariann <mariann.bogdan@petrik.hu>
2026-02-20 09:27:42 +0100

    NovenyFaj osztaly kész.

--- UveghazProjekt_/NovenyFaj.cs
+++ UveghazProjekt_/NovenyFaj.cs
@@ -8,5 +8,36 @@ namespace UveghazProjekt_
 {
 	internal class NovenyFaj
 	{
+		string nev;
+		int nedvessegtartalom; // 100 az ideális
+		int egeszsegSzint;      //100 a max,  
+		int optimalisSuruseg; 
+
+		public NovenyFaj(string nev, int optimalisSuruseg)
+		{
+			this.nev = nev;
+			this.optimalisSuruseg = optimalisSuruseg;
+			this.nedvessegtartalom = 100;
+			this.egeszsegSzint = 100;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Nedvessegtartalom { get => nedvessegtartalom; set => nedvessegtartalom = value; }
+		public int EgeszsegSzint { get => egeszsegSzint; set => egeszsegSzint = value; }
+		public int OptimalisSuruseg { get => optimalisSuruseg; set => optimalisSuruseg = value; }
+	
+		public string Azonosito
+		{
+			get
+			{
+				return this.nev.Substring(0, 3);
+			}
+		}
+		public override string ToString()
+		{
+			return $"{this.nev} nedvesseg: {this.nedvessegtartalom} egeszseg: {this.egeszsegSzint} ideális egyedszam: {this.OptimalisSuruseg}";
+		}
+
+
 	}
 }
